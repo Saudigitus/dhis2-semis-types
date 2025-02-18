163785627f9b40398a308753a1c3f3c46be02478
cnandza <cnandza@saudigitus.org>
2025-02-18 11:23:47 +0200

    adding modules enum to types

---
+++ src/types/variables/SemisTypes.ts
@@ -0,0 +1,7 @@
+export enum Modules {
+    Enrollment = "enrollment",
+    Performance = "performance",
+    Final_Result = "final_result",
+    Transfer = "transfer",
+    Attendance = "attendance"
+}
